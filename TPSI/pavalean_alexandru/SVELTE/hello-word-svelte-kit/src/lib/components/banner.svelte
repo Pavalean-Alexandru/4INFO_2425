1869c3f5d419082051f83860c8d02f4befe4380c
Pavalean-Alexandru <alexandru.pavalean@istitutoagnelli.it>
2025-03-21 13:45:15 +0100

    consegna esercizi

---
+++ TPSI/pavalean_alexandru/SVELTE/hello-word-svelte-kit/src/lib/components/banner.svelte
@@ -0,0 +1,13 @@
+<h1>My name is John Doe!</h1>
+
+<style>
+  h1 {
+    width: 30%;
+    border: 20px solid green;
+    border-radius: 20px;
+    color: red;
+    text-align: center;
+    margin: 30px auto;
+    padding: 20px;
+  }
+</style>
---
+++ TPSI/pavalean_alexandru/SVELTE/hello-word-svelte-kit/src/routes/about/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Banner from "$lib/components/banner.svelte";
+</script>
+
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>About this app</h1>
+
+	<Banner />
+</div>
