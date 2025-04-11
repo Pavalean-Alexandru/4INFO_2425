47dbaba98e5f9c4083ff0e217e9212e5dbfb289a
Pavalean-Alexandru <alexandru.pavalean@istitutoagnelli.it>
2025-04-11 11:11:27 +0200

    consegna WebApp

---
+++ TPSI/pavalean_alexandru/SVELTE/WebApp/src/lib/components/who_am_i.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let nome = "Mario";
+    export let cognome = "Rossi";
+    export let eta = 20;
+    export let width = 20
+</script>
+
+<h1>Ciao io sono {nome} {cognome} ed ho {eta} anni</h1>
+<h4>Sono uno studente presso l'istituto Agnelli a Torino</h4>
+
+<img src={'https://static.open.online/wp-content/uploads/2021/08/CR7-1.jpg'} alt="Alexandru" width={width}>
---
+++ TPSI/pavalean_alexandru/SVELTE/WebApp/src/routes/chi_sono/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+    import WhoAmI from "../../lib/components/who_am_i.svelte"
+</script>
+
+<WhoAmI nome="Alexandru" cognome="Pavalean" eta="17" width=400/>
